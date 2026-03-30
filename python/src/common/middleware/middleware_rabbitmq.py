c2e5be7d7890544a809f0389eb4e847a705a2551
Mateo Riat <mriat@fi.uba.ar>
2026-03-30 18:47:11 -0300

    refactor: abstraigo la consntante del EXCHANGE_TYPE, ademas no utilizo Random sino que dejo que RabbitMQ le asigne un nombre a la cola aleatoria

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -32,18 +32,23 @@ class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
         return wrapper
 
 class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
+    _EXCHANGE_TYPE = 'topic'
     
     def __init__(self, host, exchange_name, routing_keys):
         self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
         self.channel = self.connection.channel()
-        self.channel.exchange_declare(exchange=exchange_name, exchange_type='topic')
+        self.channel.exchange_declare(exchange=exchange_name, exchange_type=self._EXCHANGE_TYPE)
         self.exchange_name = exchange_name
         self.routing_keys = routing_keys
-        self.queue_name = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
-        self.channel.queue_declare(queue=self.queue_name, exclusive=True)
+        result = self.channel.queue_declare(queue='', exclusive=True)
+        self.queue_name = result.method.queue 
         for routing_key in routing_keys:
-            self.channel.queue_bind(exchange=exchange_name, queue=self.queue_name, routing_key=routing_key)
-    
+            self.channel.queue_bind(
+                exchange=exchange_name, 
+                queue=self.queue_name, 
+                routing_key=routing_key
+            )
+            
     def send(self, message):
         for routing_key in self.routing_keys:
             self.channel.basic_publish(exchange=self.exchange_name, routing_key=routing_key, body=message)
