ef39c7d2e9d5e9aebad4537e9a1fd2f6df332f8b
Mateo Riat <mriat@fi.uba.ar>
2026-03-30 23:34:21 -0300

    refactor: elimino la duplicacion de codigo, utilizando MessageMiddlewareQueue en Exchange

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -1,15 +1,12 @@
 import pika
-import random
-import string
 from .middleware import MessageMiddlewareQueue, MessageMiddlewareExchange
 
 class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
-
     def __init__(self, host, queue_name):
         self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
         self.channel = self.connection.channel()
-        self.channel.queue_declare(queue=queue_name)
-        self.queue_name = queue_name
+        res = self.channel.queue_declare(queue=queue_name, exclusive=(queue_name == ''))
+        self.queue_name = res.method.queue
 
     def start_consuming(self, on_message_callback):
         self.channel.basic_consume(queue=self.queue_name, on_message_callback=self._callback_wrapper(on_message_callback))
@@ -26,8 +23,8 @@ class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
     
     def _callback_wrapper(self, on_message_callback):
         def wrapper(ch, method, properties, body):
-            ack = lambda:ch.basic_ack(delivery_tag=method.delivery_tag)
-            nack = lambda:ch.basic_nack(delivery_tag=method.delivery_tag)
+            ack = lambda: ch.basic_ack(delivery_tag=method.delivery_tag)
+            nack = lambda: ch.basic_nack(delivery_tag=method.delivery_tag)
             on_message_callback(body, ack, nack)
         return wrapper
 
@@ -40,12 +37,11 @@ class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
         self.channel.exchange_declare(exchange=exchange_name, exchange_type=self._EXCHANGE_TYPE)
         self.exchange_name = exchange_name
         self.routing_keys = routing_keys
-        result = self.channel.queue_declare(queue='', exclusive=True)
-        self.queue_name = result.method.queue 
+        self._receiver_queue = MessageMiddlewareQueueRabbitMQ(host, '')
         for routing_key in routing_keys:
-            self.channel.queue_bind(
+            self._receiver_queue.channel.queue_bind(
                 exchange=exchange_name, 
-                queue=self.queue_name, 
+                queue=self._receiver_queue.queue_name, 
                 routing_key=routing_key
             )
             
@@ -54,21 +50,11 @@ class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
             self.channel.basic_publish(exchange=self.exchange_name, routing_key=routing_key, body=message)
     
     def start_consuming(self, on_message_callback):
-        self.channel.basic_consume(queue=self.queue_name, on_message_callback=self._callback_wrapper(on_message_callback))
-        self.channel.start_consuming()
+        self._receiver_queue.start_consuming(on_message_callback)
 
     def stop_consuming(self):
-        self.channel.stop_consuming()
+        self._receiver_queue.stop_consuming()
 
     def close(self):
+        self._receiver_queue.close()
         self.connection.close()
-
-    def _callback_wrapper(self, on_message_callback):
-        def wrapper(ch, method, properties, body):
-            ack = lambda:ch.basic_ack(delivery_tag=method.delivery_tag)
-            nack = lambda:ch.basic_nack(delivery_tag=method.delivery_tag)
-            on_message_callback(body, ack, nack)
-        return wrapper
-
-
-    
